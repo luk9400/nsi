81fc60cc3aa2ff6d56fc64dc74c49d2ac173ab5f
luk9400 <lukaszbratos@gmail.com>
2019-03-15 14:42:20 +0100

    fix

---
+++ list1/task1/src/smallest_factor.ads
@@ -0,0 +1,13 @@
+package Smallest_Factor
+with SPARK_Mode
+is
+  procedure SmallestF (N : in out Positive; Factor : out Positive)
+    with
+      SPARK_Mode,
+      Pre => N > 1,
+       Post => (Factor > 1) and
+     (N'Old / Factor = N) and
+      (N'Old rem Factor = 0) and
+    (for all J in 2 .. Factor - 1 => N'Old rem J /= 0);
+
+end Smallest_Factor;
