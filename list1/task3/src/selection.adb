821534f38ae1d0bf8525612a2f2e2d13c844ea10
luk9400 <lukaszbratos@gmail.com>
2019-03-18 00:21:23 +0100

    fix

--- list1/task3/src/selection.adb
+++ list1/task3/src/selection.adb
@@ -1,14 +1,4 @@
 package body Selection with SPARK_Mode is
-  function Sorted (A: Arr) return Boolean is
-  begin
-    for I in Integer range A'First .. A'Last - 1 loop
-      if A (I) >= A(I + 1) then
-        return False;
-      end if;
-    end loop;
-
-    return True;
-  end Sorted;
 
   procedure Sort (A: in out Arr) is
     Min: Integer;
