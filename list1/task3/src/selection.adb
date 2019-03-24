f86ff96099ae42781c28fc9f088fea4befefeedc
luk9400 <lukaszbratos@gmail.com>
2019-03-24 21:37:26 +0100

    grrrrrr

--- list1/task3/src/selection.adb
+++ list1/task3/src/selection.adb
@@ -12,9 +12,10 @@ package body Selection with SPARK_Mode is
     elsif A'Length > 2 then
       for I in Integer range A'First .. A'Last - 1 loop
         Min := I;
-        pragma Loop_Invariant (for all I in A'First .. A'Last => Min >= A'First and Min <= A'Last);
+        --pragma Loop_Invariant (for all I in A'First .. A'Last => Min >= A'First and Min <= A'Last);
+        --pragma Loop_Invariant (for all K in A'First .. I => A (K) <= A (I));
         for J in Integer range I + 1 .. A'Last loop
-
+          pragma Loop_Invariant (for all J in I + 1 .. A'Last => Min >= I - 1 and then Min >= A'First and then Min < A'Last);
           if A (J) < A (Min) then
             Min := J;
           end if;
