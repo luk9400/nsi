e5070230738707cdb7ecb4a34943ef5721eaa4ad
luk9400 <lukaszbratos@gmail.com>
2019-03-26 10:53:43 +0100

    fix

--- list1/task2/src/poly.adb
+++ list1/task2/src/poly.adb
@@ -2,24 +2,15 @@ with Ada.Text_IO;
 package body Poly with SPARK_Mode is
    function Horner (X: Integer; A: Vector) return Integer is
     Acc: Integer := 0;
-    Aux: Integer with Ghost;
-    Pow: Integer with Ghost;
+    Aux: Integer := 0 with Ghost;
+
   begin
-    Aux := 0;
-    if A'Length > 0 then
-      Pow := A'Length - 1;
       for Index in reverse A'First .. A'Last loop
         Acc := A (Index) + (Acc * X);
-        -- Pow bedzie ujemne dopiero na koncu ostatniej iteracji, wiec X ** -1 nigdy sie nie wykona
-        Aux := Aux + A (Index) * (X ** Pow);
-        Pow := Pow - 1;
-      end loop;
+        Aux := Aux + A (Index) * (X ** (Index - A'First));
 
-      if X = 0 then
-        Acc := A (A'First);
-        Aux := A (A'First);
-      end if;
-    end if;
+        pragma Loop_Invariant (Aux = ((X**(Index - A'First)) * Acc));
+      end loop;
 
     pragma Assert(Aux = Acc);
     return Acc;
