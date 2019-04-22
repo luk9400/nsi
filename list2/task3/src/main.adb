3f2017e540545e6ebb7407d2773625e57dc5862a
luk9400 <lukaszbratos@gmail.com>
2019-04-22 20:50:32 +0200

    t3 implementation

--- list2/task3/src/main.adb
+++ list2/task3/src/main.adb
@@ -1,6 +1,9 @@
+with Ada.Text_IO;
+with Square_Root;
 procedure Main is
 
 begin
-   --  Insert code here.
-   null;
+
+  Ada.Text_IO.Put_Line (Square_Root.Sqrt(2.0, 0.00001)'Image);
+
 end Main;
--- list2/task3/src/square_root.adb
+++ list2/task3/src/square_root.adb
@@ -1,5 +1,20 @@
 package body Square_Root with SPARK_Mode is
 
-   
+  function Sqrt (X: Float; Tolerance: Float) return Float is
+    A: Float := X / 2.0;
+  begin
+    if X /= 0.0 then
+      while abs(X/A - A) > Tolerance loop
+        A := (X/A + A) / 2.0;
+        pragma Loop_Invariant (A > 0.0);
+      end loop;
+    
+      return A;
+    else
+      return 0.0;
+    end if;
+      
+  end Sqrt;
+  
 
 end Square_Root;
