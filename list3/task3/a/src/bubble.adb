0a4d3d1b838f0a0d7e1c08c401a87da2e06b733c
luk9400 <lukaszbratos@gmail.com>
2019-05-27 18:08:16 +0200

    task3

--- list3/task3/a/src/bubble.adb
+++ list3/task3/a/src/bubble.adb
@@ -3,17 +3,24 @@ package body Bubble with SPARK_Mode is
   procedure Sort (A : in out Arr) is
     Tmp : Integer;
   begin
-    for I in reverse A'First .. A'Last - 1 loop
-      for J in A'First .. I loop
+    Outer: for I in reverse A'First .. A'Last - 1 loop
+      Inner: for J in A'First .. I loop
         if A(J) > A(J + 1) then
           Tmp := A(J);
           A(J) := A(J + 1);
           A(J + 1) := Tmp;
         end if;
         
-      end loop;
+        pragma Loop_Invariant (for all K1 in J .. J + 1 => (for some K2 in A'Range => A(K1) = A'Loop_Entry(Inner)(K2)));
+        pragma Loop_Invariant (Bubbled (A (A'First .. J + 1)));
+        pragma Loop_Invariant (for all K1 in A'First .. I + 1 => (for some K2 in A'First .. I + 1 => A(K1) = A'Loop_Entry(Inner)(K2)));
+        pragma Loop_Invariant (for all K in I + 2 .. A'Last => A(K) = A'Loop_Entry(Inner)(K));
+      end loop Inner;
       
-    end loop;
+      pragma Loop_Invariant (Sorted (A (I .. A'Last)));
+      pragma Loop_Invariant (Bubbled (A (A'First .. I + 1)));
+      
+    end loop Outer;
   end Sort;  
 
 end Bubble;
--- list3/task3/c/src/bubble.adb
+++ list3/task3/c/src/bubble.adb
@@ -3,17 +3,21 @@ package body Bubble with SPARK_Mode is
   procedure Sort (A : in out Arr) is
     Tmp : Integer;
   begin
-    for I in reverse A'First .. A'Last - 1 loop
-      for J in A'First .. I loop
+    Outer: for I in reverse A'First .. A'Last - 1 loop
+      Inner: for J in A'First .. I loop
         if A(J) > A(J + 1) then
           Tmp := A(J);
           A(J) := A(J + 1);
           A(J + 1) := Tmp;
         end if;
         
-      end loop;
+        pragma Loop_Invariant (for all K1 in A'Range => (for some K2 in A'Range => A(K2) = A'Loop_Entry(Inner)(K1)));
+        
+      end loop Inner;
       
-    end loop;
+      pragma Loop_Invariant (for all K1 in A'Range => (for some K2 in A'Range => A(K2) = A'Loop_Entry(Outer)(K1)));
+      
+    end loop Outer;
   end Sort;  
 
 end Bubble;
